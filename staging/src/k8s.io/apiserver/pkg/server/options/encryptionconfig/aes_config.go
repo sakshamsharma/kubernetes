f838eb6f1dbb881a565466fabc6156877cc59617
Saksham Sharma <saksham0808@gmail.com>
2017-05-31 10:10:39 -0700

    Parse configuration using a union struct

--- staging/src/k8s.io/apiserver/pkg/server/options/encryptionconfig/aes_config.go
+++ staging/src/k8s.io/apiserver/pkg/server/options/encryptionconfig/aes_config.go
@@ -32,14 +32,27 @@ type Key struct {
 }
 
 // Config contains the API configuration for an AES transformer
-type AesConfig struct {
+type AESConfig struct {
 	Keys []Key `json:"keys"`
 }
 
-// GetPrefixTransformer implements the TransformerConfig interface for AesConfig
-func (config AesConfig) GetPrefixTransformer() (value.PrefixTransformer, error) {
-	keyTransformers := []value.PrefixTransformer{}
+// GetPrefixTransformer implements the TransformerConfig interface for AESConfig
+func (config *AESConfig) GetPrefixTransformer() (value.PrefixTransformer, error) {
 	var result value.PrefixTransformer
+
+	if len(config.Keys) == 0 {
+		return result, fmt.Errorf("aes provider has no valid keys")
+	}
+	for _, key := range config.Keys {
+		if key.Name == "" {
+			return result, fmt.Errorf("key with invalid name provided")
+		}
+		if key.Secret == "" {
+			return result, fmt.Errorf("key %v has no provided secret", key.Name)
+		}
+	}
+
+	keyTransformers := []value.PrefixTransformer{}
 
 	for _, keyData := range config.Keys {
 		key, err := base64.StdEncoding.DecodeString(keyData.Secret)
@@ -61,7 +74,7 @@ func (config AesConfig) GetPrefixTransformer() (value.PrefixTransformer, error) 
 
 	// Create a prefixTransformer which can choose between these keys
 	keyTransformer := value.NewPrefixTransformers(
-		fmt.Errorf("no matching key was found for the provided AEAD transformer"), keyTransformers...)
+		fmt.Errorf("no matching key was found for the provided AES transformer"), keyTransformers...)
 
 	// Create a PrefixTransformer which shall later be put in a list with other providers
 	result = value.PrefixTransformer{
@@ -71,3 +84,10 @@ func (config AesConfig) GetPrefixTransformer() (value.PrefixTransformer, error) 
 
 	return result, nil
 }
+
+// IsNil implements the TransformerConfig interface for AESConfig
+func (config *AESConfig) IsNil() bool {
+	return config == nil
+}
+
+var _ TransformerConfig = &AESConfig{}
