8af87b12487acb7f8e7c8fe2615972237652ff0e
Saksham Sharma <saksham0808@gmail.com>
2017-07-25 16:46:21 -0700

    Add KeyManagementService to gce cloudprovider

--- cluster/gce/gci/configure-helper.sh
+++ cluster/gce/gci/configure-helper.sh
@@ -402,6 +402,16 @@ EOF
 subnetwork-name = ${NODE_SUBNETWORK}
 EOF
     fi
+
+    # Assign variables needed to use a Google Cloud KMS key.
+    # They need not be provided by the cloud, and thus default to empty fields
+    # so the cloud configuration can get parsed by gcfg without an error.
+    # Missing values will be handled by kms.go.
+    cat <<EOF >>/etc/gce.conf
+kms-location = ${GKMS_LOCATION:-}
+kms-keyRing = ${GKMS_KEYRING:-}
+kms-cryptoKey = ${GKMS_CRYPTOKEY:-}
+EOF
   fi
   if [[ -n "${NODE_INSTANCE_PREFIX:-}" ]]; then
     use_cloud_config="true"
